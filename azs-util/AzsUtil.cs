f3f4da6fe3e1b54b402241d21514b827fc142ebf
lucasvogel <lucas.vogel@endpointsystems.com>
2024-04-30 21:34:34 -0400

    depups and annoying header cleanup

--- azs-util/AzsUtil.cs
+++ azs-util/AzsUtil.cs
@@ -1,6 +1,6 @@
 namespace azs_util;
 
-[Command(Name = "azs-util", Description = "an Azure storage account utility for blob and table storage")]
+[Command(Name = "azs-util", Description = "an Azure storage account utility for blob and table storage.\r\nEndpoint Systems\r\nhttps://endpointsystems.com\r\nFind this tool on GitHub at https://github.com/endpointsystems/azs-util")]
 [Subcommand(typeof(Table.Table), typeof(Containers.Containers))]
 public class AzsUtil
 {
--- azs-util/Program.cs
+++ azs-util/Program.cs
@@ -3,13 +3,7 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
-        Console.WriteLine("---------------------");
-        Console.WriteLine("azs-util: an Azure storage account utility for blob and table storage");
-        Console.WriteLine("by Endpoint Systems");
-        Console.WriteLine("https://endpointsystems.com");
-        Console.WriteLine("Find this tool on GitHub at https://github.com/endpointsystems/azs-util");
-        Console.WriteLine("---------------------");
-        var app = new CommandLineApplication()
+        var app = new CommandLineApplication
         {
             Name = "azs-util",
             Description = "an Azure storage account utility for blob and table storage"
